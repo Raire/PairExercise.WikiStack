16817a179079d644684dd47ac0dc20d59d63615b
AsherLMac <renpsybeta@gmail.com>
2018-06-15 11:56:04 -0400

    In-Progess CK1

---
+++ app.js
@@ -0,0 +1,20 @@
+const morgan = require('morgan')
+const express = require('express')
+const bodyParser = require('body-parser')
+const layout = require('./views/layout.js')
+
+const app = express()
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.get(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.send(layout(''));
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+});
